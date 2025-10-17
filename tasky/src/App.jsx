0b32911091a7d05a2ab9e676fb91e7a917e26fb6
KyleG4 <20107489@setu.ie>
2025-10-17 19:35:19 +0100

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,22 +5,30 @@ import Task from './components/Task';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" },
-      { id: 3, title: "Tidy up", description: "", deadline: "Today", priority: "High" }
+      { id: 1, title: "Dishes",  description: "Empty dishwasher",           deadline: "Today",    priority: "Low",    done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away",  deadline: "Tomorrow", priority: "Medium", done: false },
+      { id: 3, title: "Tidy up", description: "",                           deadline: "Today",    priority: "High",   done: false }
     ]
   });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({ tasks });
+  };
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (
+      {taskState.tasks.map((task, index) => (
         <Task
           key={task.id}
           title={task.title}
           description={task.description}
           deadline={task.deadline}
           priority={task.priority}
+          done={task.done}
+          markDone={() => doneHandler(index)}
         />
       ))}
     </div>
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,10 +1,11 @@
-function Task({ title, description, deadline, priority }) {
+function Task({ title, description, deadline, priority, done, markDone }) {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: done ? 'lightgrey' : '#5bb4c4' }}>
       <p className="title">{title}</p>
       <p>Due: {deadline}</p>
       {description && <p><em>{description}</em></p>}
-      <p className="priority">{priority}</p>
+      {priority && <p className="priority">{priority}</p>}
+      <button onClick={markDone} className="doneButton">Done</button>
     </div>
   );
 }
